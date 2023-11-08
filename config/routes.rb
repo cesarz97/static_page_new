af229eb1993018ccec27a9c443a4c5d15b4b6d0f
cesarz97 <lookout5mahesh@gmail.com>
2023-11-08 11:57:45 +0530

    login controller and routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  
+  resources :users
+
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
+  post '/login',to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
